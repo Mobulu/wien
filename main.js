bbaa613acc37a16c44e268b97565af69f29c9d76
Mario Lehmeyer <mario.lehmeyer@student.uibk.ac.at>
2022-04-28 17:59:12 +0200

    Maßstab hinzugefügt

--- main.js
+++ main.js
@@ -39,3 +39,6 @@ layerControl.addOverlay(sightLayer, "Sehenswürdigkeiten");
 let mrk = L.marker([ stephansdom.lat, stephansdom.lng]).addTo(sightLayer);
 
 sightLayer.addTo(map);
+
+// Maßstab hinzufügen
+L.control.scale().addTo(map);
