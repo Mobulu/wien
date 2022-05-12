ae760f9e8b14249fa57efdb9514dba900cfd44f2
Mario Lehmeyer <mario.lehmeyer@student.uibk.ac.at>
2022-05-12 15:45:10 +0200

    Liniennetz Popup implementiert

--- main.js
+++ main.js
@@ -130,8 +130,17 @@ async function loadLines(url) {
     layerControl.addOverlay(overlay, "Liniennetz Vienna Sightseeing");
     overlay.addTo(map); //damit wird entschieden ob die Checkbox standardmäßig an- oder ausgeschaltet ist
 
-    L.geoJSON(geojson).addTo(overlay);
+    L.geoJSON(geojson).bindPopup(function (layer) {
+    return `
+        <h4>${layer.feature.properties.LINE_NAME}</h4>
+        von: ${layer.feature.properties.FROM_NAME}
+        <br>
+        nach: ${layer.feature.properties.TO_NAME}
+    `;
+        //return layer.feature.properties.LINE_NAME;
+}).addTo(map);
 }
+
 loadLines ("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json");
 
 // Fußgängerzonen
